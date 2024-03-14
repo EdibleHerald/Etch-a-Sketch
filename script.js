0ef8c94751decbf21a2e97e1581bb27561d5e0d5
EdibleHerald <142949302+EdibleHerald@users.noreply.github.com>
2024-03-14 18:49:03 -0400

    Summarized assignment in JS comments

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // Builds a browser verison of something between a sketch pad
 // and an Etch-a-Sketch
 
-// 
+// Create a webpage with a 16x16 grid of square grids
+// NOTE: USE Javascript TO CREATE DIVS
+// NOTE: PUT SAID DIVS INSIDE OF A CONTAINER DIV (Can be added using html)
+
+// Use flexbox to make divs appear as a grid
+// Setup a "hover" effeect so that the grid divs change color
+// when mouse passes over them, leaving a trail
+
+// Create a popup that asks the user how many squares per side
+// Should generate a new grid with the same space as the 16x16
+// NOTE: Set MAXIMUM as 100(100x100)
